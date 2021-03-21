5aacf592a1917009fdb2678d9ac45593858d8ed9
pedrorcz <pedro400012@gmail.com>
2021-03-21 12:32:17 -0500

    Agregar y Estructura de Ingreso Producto

---
+++ src/app/ingreso-gasto/ingreso-gasto.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IngresoGastoComponent } from './ingreso-gasto.component';
+
+describe('IngresoGastoComponent', () => {
+  let component: IngresoGastoComponent;
+  let fixture: ComponentFixture<IngresoGastoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IngresoGastoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IngresoGastoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/ingreso-gasto/ingreso-gasto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-gasto',
+  templateUrl: './ingreso-gasto.component.html',
+  styleUrls: ['./ingreso-gasto.component.css']
+})
+export class IngresoGastoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
