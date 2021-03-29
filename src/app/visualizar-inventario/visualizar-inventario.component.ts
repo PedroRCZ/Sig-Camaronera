cc08aca82b78577d39cb623435300d8073864258
pedrorcz <pedro400012@gmail.com>
2021-03-29 14:23:23 -0500

    Visualizar inventario

--- src/app/visualizar-inventario/visualizar-inventario.component.ts
+++ src/app/visualizar-inventario/visualizar-inventario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Cosumo } from '../models/consumo';
+import { Gastos } from '../models/gastos';
+import { ConsumoServices } from '../services/consumo.service';
+import { GastosServices } from '../services/gastos.servive';
 
 @Component({
   selector: 'app-visualizar-inventario',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizarInventarioComponent implements OnInit {
 
-  constructor() { }
+  listGastos: Gastos[] = [];
+  listGastos1: Gastos[] = [];
+  listConsumo: Cosumo[] = [];
+  listConsumo1: Cosumo[] = [];
+
+  constructor(private _gastoservices: GastosServices,
+              private _consumoservices: ConsumoServices) { }
 
   ngOnInit(): void {
+    this.obtenerConsumo();
+    this.obtenerGastos();
+    
   }
+
+  obtenerGastos(){
+    this._gastoservices.getVerGastos().subscribe( data => {
+      console.log(data);
+      this.listGastos = data;
+    }, error => {
+      console.log(error);
+    })
+        
+  }
+  obtenerConsumo(){ //cambiar
+    this._consumoservices.getVerConsumo().subscribe( data => {
+      console.log(data);
+      this.listConsumo = data;
+    }, error => {
+      console.log(error);
+    })
+    
+  }
+
+  
 
 }
