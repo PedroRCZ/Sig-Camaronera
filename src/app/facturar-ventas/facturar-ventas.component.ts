60968b3efa9a9a94e39e3e8cbcf6f9631bec48a2
pedrorcz <pedro400012@gmail.com>
2021-03-29 13:36:29 -0500

    facturar ventas

--- src/app/facturar-ventas/facturar-ventas.component.ts
+++ src/app/facturar-ventas/facturar-ventas.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteServices } from '../services/cliente.service';
+import { ToastrService } from 'ngx-toastr';
+import { Cliente } from '../models/cliente';
+import { CamaronServices } from '../services/camaron.service';
+import { Camaron } from '../models/camaron';
+import { Factura } from '../models/facturaAgregar';
+import { VerVentasServices } from '../services/ver-venta.service';
 
 @Component({
   selector: 'app-facturar-ventas',
@@ -7,9 +15,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacturarVentasComponent implements OnInit {
 
-  constructor() { }
+  facturarFrom: FormGroup;
+  listCliente: Cliente[] = [];
+  listCamaron: Camaron[] = [];
+  //CLiente
+  idcli: string = "";
+  tecli: number = 0;
+  dicli: string = "";
+  cicli: string = "";
+  nomcli: string = "";
+  //Camaron
+  idCamaron: string = "";
+  tallaCam: number = 0;
+  pesoCam: number = 0;
+  // factura
+  date = new Date();
+  Iva : number = 0.12;
+  sub_t : number = 0;
+  cantidad_lb: number = 0;
+  precio_cam: number = 0;
+  total_fac: number = 0;
+
+  constructor(private _clienteservices: ClienteServices,
+              private _camaronservices: CamaronServices,
+              private _verventasservices: VerVentasServices,
+              private fb: FormBuilder,
+              private toastr: ToastrService) { 
+                this.facturarFrom = this.fb.group({
+                  id_factura: ['', Validators.required],
+                  fecha_factura: ['', Validators.required],
+                  id: ['', Validators.required],
+                  nombre: [{value: '' , disabled: true}, Validators.required],
+                  ciudad: [{value: '' , disabled: true}, Validators.required],
+                  direcc: [{value: '' , disabled: true}, Validators.required],
+                  tel: [{value: '' , disabled: true}, Validators.required],
+                  idCam: ['', Validators.required],
+                  talla: [{value: '' , disabled: true}, Validators.required],
+                  peso: [{value: '' , disabled: true}, Validators.required],
+                  precio: ['', Validators.required],
+                  cantidad: ['', Validators.required],
+                  sub_total: [{value: '' , disabled: true}, Validators.required],
+                  IVA: [{value: '' , disabled: true}, Validators.required],
+                  total: [{value: '' , disabled: true}, Validators.required],
+                })
+              }
+
 
   ngOnInit(): void {
   }
 
+  agregarFactura(){
+    const FACTURA: Factura = {
+      factura_id: this.facturarFrom.get('id_factura')?.value, 
+      factura_fecha: this.facturarFrom.get('fecha_factura')?.value, 
+      cliente_id: this.facturarFrom.get('id')?.value, 
+      camaron_id: this.facturarFrom.get('idCam')?.value, 
+      factura_precio_camaron: this.facturarFrom.get('precio')?.value, 
+      factura_subtotal: this.cantidad_lb * this.precio_cam,
+      factura_iva: this.facturarFrom.get('IVA')?.value * 100, 
+      factura_precio_final: this.cantidad_lb * this.precio_cam * this.Iva + (this.cantidad_lb * this.precio_cam),
+      monto_peso: this.facturarFrom.get('cantidad')?.value,
+    }
+    console.log(FACTURA);
+    this._verventasservices.guardarVentas(FACTURA).subscribe( data => {
+      console.log(data)
+      this.toastr.success('El Factura fue registrado con exito', 'Factura Registrado');
+      this.facturarFrom.reset();
+    },error =>{
+      console.log(error);
+      this.toastr.error('El Factura No fue registrado con exito', 'Factura No Registrado');
+      this.facturarFrom.reset();
+    })
+  }
+
+  consultarCamaron(){
+    this._camaronservices.getCamaronById(this.facturarFrom.get('idCam')?.value).subscribe( data1 =>{
+      if(Object.keys(data1).length === 0){
+        console.log(this.facturarFrom.get('id')?.value)
+        this.toastr.error('El Camaron No se encuentra registrado', 'Camaron No Registrado');
+        this.idCamaron = "";
+        this.tallaCam = 0;
+        this.pesoCam = 0;
+      }else{
+        this.toastr.success('El Camaron fue consultado con exito', 'Camaron Exite');
+        this.listCamaron = data1;
+        this.idCamaron = this.listCamaron[0].camaron_id + "";
+        this.tallaCam = this.listCamaron[0].camaron_talla;
+        this.pesoCam = this.listCamaron[0].camaron_peso;
+        console.log(data1);
+      } 
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  consultarCliente(){
+    this._clienteservices.getClienteByID(this.facturarFrom.get('id')?.value).subscribe( data2 => { 
+      if(Object.keys(data2).length === 0){
+        console.log(this.facturarFrom.get('id')?.value)
+        this.toastr.error('El Cliente No se encuentra registrado', 'Cliente No Registrado');
+        this.idcli = "";
+        this.tecli = 0;
+        this.dicli = "";
+        this.cicli = "";
+        this.nomcli = "";
+      }else{
+        this.toastr.success('El Cliente fue consultado con exito', 'Cliente Exite');
+        this.listCliente = data2;
+        this.idcli = this.listCliente[0].cliente_id + "";
+        this.tecli = this.listCliente[0].cliente_telefono;
+        this.dicli = this.listCliente[0].cliente_direccion;
+        this.cicli = this.listCliente[0].cliente_ciudad;
+        this.nomcli =this.listCliente[0].cliente_nombre;
+        console.log(data2);
+      } 
+    }, error => {
+      console.log(error);
+    })
+  }
 }
